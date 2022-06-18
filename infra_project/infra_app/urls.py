99694159c81297c22e42914ccfb85928f78314af
Vladimir Vasilev <vasilevvladv@yandex.ru>
2022-06-18 17:28:49 +0400

    Fix flask9

--- infra_project/infra_app/urls.py
+++ infra_project/infra_app/urls.py
@@ -3,9 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = 'infra_app'
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('second/', views.second_page, name='second_page'),
-
 ]
